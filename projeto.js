77a4f0cfb2b671ef150e906c3866853d9d970415
Leojjackson <leojjackson@outlook.com>
2022-11-11 20:30:08 -0300

    Updated 11/11

---
+++ projeto.js
@@ -0,0 +1,11 @@
+// function clicar()
+// {
+//     let ema = document.getElementById('#email');
+//     let se = document.getElementById('#senha');
+//     let email = ema.Value;
+//     let senha = se.value;
+//     if(email == 'leojjackson@outlook.com' || email == 'leojjackson87@gmail.com')
+//     {
+
+//     }
+// }
