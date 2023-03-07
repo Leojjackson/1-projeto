24c5ba8a6aac55d041d3cfd5c1fdb662cffdcade
Leojjackson <leojjackson@outlook.com>
2023-03-07 18:51:30 -0300

    07/03/2023

--- projeto.js
+++ projeto.js
@@ -1,11 +1,11 @@
-// function clicar()
-// {
-//     let ema = document.getElementById('#email');
-//     let se = document.getElementById('#senha');
-//     let email = ema.Value;
-//     let senha = se.value;
-//     if(email == 'leojjackson@outlook.com' || email == 'leojjackson87@gmail.com')
-//     {
-
-//     }
-// }
+let ema = document.getElementById('email');
+let se = document.getElementById('senha');
+let email = ema.Value;
+let senha = se.value;
+function clicar()
+{
+    if(email == 'leojjackson@outlook.com' || email == 'leojjackson87@gmail.com')
+    {
+        console.log("worked...")
+    }
+}
