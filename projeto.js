7e28598d106817c8627a4790d1170d26dad2a52b
Leojjackson <leojjackson@outlook.com>
2023-03-07 19:34:29 -0300

    07/03/2023

--- projeto.js
+++ projeto.js
@@ -5,9 +5,7 @@ let confirmar = document.querySelector("confirmar");
 function clicar(){
 
     if(email.value == "leojjackson@outlook.com" && senha.value == 123456){
-
         console.log("worked...")
-
     }
     else{
         console.log("didn't work")
