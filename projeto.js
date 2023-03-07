51b8935dea00b7cc7325845cfb8c2d73b14b43a8
Leojjackson <leojjackson@outlook.com>
2023-03-07 19:28:42 -0300

    JavaScript adicionado

--- projeto.js
+++ projeto.js
@@ -2,8 +2,11 @@ let email = document.querySelector("#email");
 let senha = document.querySelector("#senha");
 let confirmar = document.querySelector("confirmar");
 
-confirmar.addEventListener("click", (event)=>{
+function clicar(){
+
     if(email.value == "leojjackson@outlook.com" && senha.value == 123456){
+
         console.log("worked...")
+
     }
-})
+}
