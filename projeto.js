a3a089bfdc925911a53ce613d3d4122d05537d37
Leojjackson <leojjackson@outlook.com>
2023-03-13 21:53:50 -0300

    13/03/2023

--- projeto.js
+++ projeto.js
@@ -5,9 +5,11 @@ let confirmar = document.querySelector("confirmar");
 function clicar(){
 
     if(email.value == "leojjackson@outlook.com" && senha.value == 123456){
+        
         console.log("worked...")
     }
     else{
+        window.location = "http://127.0.0.1:5500/pagina2.html"
         console.log("didn't work")
     }
 }
